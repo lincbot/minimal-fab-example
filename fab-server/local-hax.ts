5860ebbcc3182c3a2b1d20f014e3c48bbbed94be
Glen Maddern <glenmaddern@gmail.com>
2020-01-23 15:39:47 +0000

    single-file "plugins" are behaving exactly correct, Morty!

--- fab-server/local-hax.ts
+++ fab-server/local-hax.ts
@@ -6,21 +6,34 @@ import {
   PluginMetadata
 } from '@fab/core'
 
+function getCurrentTime() {
+  return new Date().toLocaleString()
+}
+
 export const build: FabBuildStep = async (
   args: PluginArgs,
   proto_fab: ProtoFab
 ) => {
-  proto_fab.metadata.ts_test = args.now
+  proto_fab.metadata.local_hax = {
+    compiled_at: getCurrentTime(),
+    args
+  }
 }
 
 export const runtime: FabPluginRuntime = (
   args: PluginArgs,
   metadata: PluginMetadata
 ) => {
-  return async ({url}) => {
-    console.log("I IN THE FAB YO")
+  return async ({ url }) => {
+    console.log('I IN THE FAB YO')
     if (url.pathname === '/lol') {
-      return new Response(`HEYOOOOO`, {
+      const lines = [
+        'HEYOOOOO!',
+        `Rendered at: ${getCurrentTime()}`,
+        JSON.stringify(metadata.local_hax, null, 2)
+      ]
+
+      return new Response(lines.join('\n'), {
         status: 200
       })
     }
